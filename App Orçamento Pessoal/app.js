ab752c742cc3de046db3c0cc444c164bdfe6bd7b
toledoneto <toledo.neto@outlook.com>
2018-08-01 16:54:41 -0300

    App despesa pessoal: validação de dados de registro

---
+++ App Orçamento Pessoal/app.js
@@ -0,0 +1,105 @@
+class Expense
+{
+
+	constructor(year, month, day, typeOf, description, cost)
+	{
+
+		this.year = year;
+		this.month = month;
+		this.day = day;
+		this.typeOf = typeOf;
+		this.description = description;
+		this.cost = cost;
+
+	}
+
+	// verificando a validade dos dados do formulário
+	validateData()
+	{
+
+		for(let i in this)
+		{
+
+			if (this[i] == undefined || this[i] == '' || this[i] == null) return false;
+
+		}
+
+		return true;
+
+	}
+
+}
+
+class Db
+{
+
+	// como ao iniciar o app não teremos um id inicial, é necessário verificar no localStorage (LS)
+	// se estamos no início do app e, em caso afirmativo, atribuir um id inicial (0 no caso)
+	constructor()
+	{
+
+		// recupera o id no LS
+		let id = localStorage.getItem('id');
+
+		if (id === null) localStorage.setItem('id', 0);
+
+	}
+	
+	// procurando o último Id registrado no localStorage para criarmos o próx id
+	getNextId()
+	{
+
+		let nextId = localStorage.getItem('id');
+		return parseInt(nextId) + 1;
+
+	}
+
+	// método para salvar no LocalStorage a despesa criada no app
+	save(expense)
+	{
+
+		// cria o próx id
+		let id = this.getNextId();
+
+		// add no LS
+		localStorage.setItem(id, JSON.stringify(expense));
+
+		// prepara o próx id a ser usado no próx cadastro
+		localStorage.setItem('id', id);
+
+	}
+
+}
+
+let db = new Db();
+
+function createExpense()
+{
+
+	let year = document.getElementById('ano');
+	let month = document.getElementById('mes');
+	let day = document.getElementById('dia');
+	let typeOf = document.getElementById('tipo');
+	let description = document.getElementById('descricao');
+	let cost = document.getElementById('valor');
+
+	let expense =  new Expense(year.value, month.value, day.value, 
+								typeOf.value, description.value, cost.value);
+
+	if(expense.validateData())	
+	{
+		
+		db.save(expense);
+
+		// um pouco de JQuery caso não haja erro no preenchimento do form
+		$('#saveSuccess').modal('show');
+
+	} else {
+
+		// um pouco de JQuery caso haja erro no preenchimento do form
+		$('#saveError').modal('show');
+
+	};
+
+}
+
